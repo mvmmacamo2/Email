a3060359a5f18b84488a9600cfe17eeb79d4d5f5
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-29 00:29:57 -0700

    Criando o Projecto de Email

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Emali</title>
+    <meta name="author" content="Miguel Vasco Macamo">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+</body>
+</html>
