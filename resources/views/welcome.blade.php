c0bc9810b75094b7a6a3dc13924ee2ab364b267e
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-29 00:37:35 -0700

    trabalhando com a tela de envio de email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,21 @@
     <title>Laravel Emali</title>
     <meta name="author" content="Miguel Vasco Macamo">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
+    <div class="flex-center position-ref full-height">
+
+        <form action="welcome_submit" method="get" accept-charset="utf-8">
+
+           <input type="email" name="">
+           <input type="text" name="title">
+           <button type="submit">Send me a Mail</button>
+
+       </form>
+
+   </div>
 
 </body>
 </html>
